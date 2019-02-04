e6d82b997b7dde7430c9dac3114c98412d5817c0
edwardwliu <edwardliu@berkeley.edu>
2019-02-04 02:15:59 -0800

    add multilayerForestry C++ graphs

--- replicationCode/2-generateEstimators.R
+++ replicationCode/2-generateEstimators.R
@@ -5,25 +5,39 @@ library(ggplot2)
 library(xgboost)
 source("src/gb_cv.R")
 source("src/gb_forestry.R")
-source("src/gb_part.R")
+source("src/gb_rpart.R")
 
 # Define all estimators:
 
 estimator_grid <- list(
-  "xgboost_1" = function(Xobs, Yobs)
-    xgboost(data = data.matrix(Xobs), label= Yobs, nrounds = 20),
-  "gb_forestry" = function(Xobs, Yobs)
-    gradient_boosting_forestry(Xobs, Yobs, n_iterations = 20, eta = 0.3)
+  "xgboost" = function(Xobs, Yobs)
+    xgboost(data = data.matrix(Xobs), label = Yobs, nrounds = 10),
+  "forestry" = function(Xobs, Yobs)
+    forestry(x = Xobs, y = Yobs, ntree = 10, replace = FALSE, 
+             sampsize = nrow(Xobs), mtry = ncol(Xobs), 
+             nodesizeStrictSpl = max(round(nrow(Xobs)/128), 1), 
+             nodesizeStrictAvg = max(round(nrow(Xobs)/128), 1), 
+             splitrule = "variance", middleSplit = TRUE, maxDepth = 6),
+  "gbForestry_r_10x10" = function(Xobs, Yobs)
+    gradient_boosting_forestry(Xobs, Yobs, ntree = 10, n_iterations = 10),
+  "multilayerForestry_cpp_10x10" = function(Xobs, Yobs)
+    multilayerForestry(x = Xobs, y = Yobs, ntree = 10, 
+                       maxDepth = 6, nrounds = 10)
 )
 
 
-
 predictor_grid <- list(
-  "xgboost_1" = function(estimator, feat) {
+  "xgboost" = function(estimator, feat) {
     feat <- data.matrix(feat)
     return(predict(estimator, feat))
   },
-  "gb_forestry" = function(estimator, feat) {
+  "forestry" = function(estimator, feat) {
+    return(predict(estimator, feat))
+  },
+  "gbForestry_r_10x10" = function(estimator, feat) {
+    return(predict(estimator, feat))
+  },
+  "multilayerForestry_cpp_10x10" = function(estimator, feat) {
     return(predict(estimator, feat))
   }
 )
